40a4c2c45aeac409071d7088cf4fa735aaffa6dc
Josh Perez <josh@goatslacker.com>
2016-08-02 00:03:06 -0700

    removals and stuff

--- index.js
+++ index.js
@@ -4,105 +4,37 @@ const Pokemon = require('./pokemon.json')
 const Moves = require('./moves.json')
 const LevelToCPM = require('./level-to-cpm.json')
 
-// CP = (BaseAtk + IndAtk) * (BaseDef + IndDef)^0.5 * (BaseSta + IndSta)^0.5 * (ECpM)^2 / 10
-
-const find = (obj, f) => Object.keys(obj).reduce((a, key) => {
-  if (a) return a
-  if (f(obj[key]) === true) return obj[key]
-  return null
-}, null)
-
 function findPokemon(name) {
   const fmtName = name.toUpperCase()
-  const mon = find(Pokemon, x => x.name === fmtName)
 
-  return mon
+  return Object.keys(Pokemon).reduce((a, key) => {
+    if (a) return a
+    if (Pokemon[key].name === fmtName) return Pokemon[key]
+    return null
+  }, null)
 }
 
-// XXX this part is really important!
 const DustToLevel = {
+  200: [1, 1.5, 2, 2.5],
+  400: [3, 3.5, 4, 4.5],
+  600: [5, 5.5, 6, 6.5],
+  800: [7, 7.5, 8, 8.5],
+  1000: [9, 9.5, 10, 10.5],
+  1300: [11, 11.5, 12, 12.5],
+  1600: [13, 13.5, 14, 14.5],
+  1900: [15, 15.5, 16, 16.5],
+  2200: [17, 17.5, 18, 18.5],
   2500: [19, 19.5, 20, 20.5],
   3000: [21, 21.5, 22, 22.5],
   3500: [23, 23.5, 24, 24.5],
   4000: [25, 25.5, 26, 26.5],
-}
-
-function stats() {
-  // These come from our Pokemon database
-  const BaseAtk = 126
-  const BaseDef = 126
-  const BaseSta = 90
-
-  // I'm trying to find these values!
-  const IndAtk = 13
-  const IndDef = 13
-  const IndSta = 14
-
-  // This needs to be user provided, and it is very important!!!!
-  const Level = 20
-
-  const ECpM = LevelToCPM[String(Level)]
-
-  const CP = Math.floor(
-    (BaseAtk + IndAtk) *
-    Math.pow((BaseDef + IndDef), 0.5) *
-    Math.pow((BaseSta + IndSta), 0.5) *
-    Math.pow(ECpM, 2) /
-    10
-  )
-  const HP = Math.floor(ECpM * (BaseSta + IndSta))
-
-  return { CP, HP }
-}
-
-function ivStaminaFromHP(hp) {
-  const BaseSta = 90
-  const Level = 20
-  const ECpM = LevelToCPM[String(Level)]
-
-  return Array.from(Array(15))
-    .map((_, i) => i)
-    .filter(IndSta => hp === Math.floor(ECpM * (BaseSta + IndSta)))
-}
-
-// This works OK except that I really can't tell attack and defense apart
-// and sometimes they might have wildly different IVs
-function ivAttackDefenseFromCP(cp, IndSta) {
-  const BaseAtk = 126
-  const BaseDef = 126
-  const BaseSta = 90
-  const Level = 20
-  const ECpM = LevelToCPM[String(Level)]
-
-  const possibleValues = []
-  for (let IndAtk = 0; IndAtk < 15; IndAtk += 1) {
-    for (let IndDef = 0; IndDef < 15; IndDef += 1) {
-      const CP_RAW = (
-        (BaseAtk + IndAtk) *
-        Math.pow((BaseDef + IndDef), 0.5) *
-        Math.pow((BaseSta + IndSta), 0.5) *
-        Math.pow(ECpM, 2) /
-        10
-      )
-      const CP = Math.floor(CP_RAW)
-      const Total = IndAtk + IndDef
-
-      const Perfect = Math.round((Total + IndSta) / 45 * 100)
-
-      if (cp === CP) {
-        possibleValues.push({
-          IndAtk,
-          IndDef,
-          IndSta,
-          Total,
-          CP_RAW,
-          Perfect
-        })
-      }
-    }
-  }
-
-  return possibleValues
+  4500: [27, 27.5, 28, 28.5],
+  5000: [29, 29.5, 30, 30.5],
+  6000: [31, 31.5, 32, 32.5],
+  7000: [33, 33.5, 34, 34.5],
+  8000: [35, 35.5, 36, 36.5],
+  9000: [37, 37.5, 38, 38.5],
+  10000: [39, 39.5, 40, 40.5],
 }
 
 function calcIndSta(hp, BaseSta, ECpM) {
@@ -111,30 +43,53 @@ function calcIndSta(hp, BaseSta, ECpM) {
     .filter(IndSta => hp === Math.floor(ECpM * (BaseSta + IndSta)))
 }
 
-function calculate(pokemonName, cp, hp, Level) {
-  const mon = find(Pokemon, x => x.name === pokemonName.toUpperCase())
-
+function getCP(mon, ivs, ECpM) {
   const BaseAtk = mon.stats.attack
   const BaseDef = mon.stats.defense
+  const BaseSta = mon.stats.stamina
+  const IndAtk = ivs.atk
+  const IndDef = ivs.def
+  const IndSta = ivs.sta
+
+  return Math.floor(
+    (BaseAtk + IndAtk) *
+    Math.pow(BaseDef + IndDef, 0.5) *
+    Math.pow(BaseSta + IndSta, 0.5) *
+    Math.pow(ECpM, 2) /
+    10
+  )
+}
+
+function maxCP(mon) {
+  return getCP(mon, {
+    atk: 15,
+    def: 15,
+    sta: 15,
+  }, 0.790300)
+}
+
+function calculate(pokemonName, cp, hp, stardust, Level) {
+  const mon = findPokemon(pokemonName)
+
   const BaseSta = mon.stats.stamina
 
   const ECpM = LevelToCPM[String(Level)]
 
-  // TODO what if this has a length of more than 1, or a length or 0?
+  if (DustToLevel[stardust].indexOf(Level) === -1) {
+    throw new Error('Stardust does not match level')
+  }
+
   const IndStaValues = calcIndSta(hp, BaseSta, ECpM)
 
   const possibleValues = []
   IndStaValues.forEach((IndSta) => {
     for (let IndAtk = 0; IndAtk <= 15; IndAtk += 1) {
       for (let IndDef = 0; IndDef <= 15; IndDef += 1) {
-        const CP_RAW = (
-          (BaseAtk + IndAtk) *
-          Math.pow((BaseDef + IndDef), 0.5) *
-          Math.pow((BaseSta + IndSta), 0.5) *
-          Math.pow(ECpM, 2) /
-          10
-        )
-        const CP = Math.floor(CP_RAW)
+        const CP = getCP(mon, {
+          atk: IndAtk,
+          def: IndDef,
+          sta: IndSta,
+        }, ECpM)
         const Total = IndAtk + IndDef
 
         const Perfect = Math.round((Total + IndSta) / 45 * 100)
@@ -146,7 +101,6 @@ function calculate(pokemonName, cp, hp, Level) {
             IndDef,
             IndSta,
             Total,
-            CP_RAW,
             Perfect,
           })
         }
@@ -157,25 +111,7 @@ function calculate(pokemonName, cp, hp, Level) {
   return possibleValues
 }
 
-function calcIVS(pokemonName, hp, Level) {
-  const mon = find(Pokemon, x => x.name === pokemonName.toUpperCase())
-
-  const BaseAtk = mon.stats.attack
-  const BaseDef = mon.stats.defense
-  const BaseSta = mon.stats.stamina
-
-  const ECpM = LevelToCPM[String(Level)]
-
-  // XXX pretty sure they pop the value...
-  return calcIndSta(hp, BaseSta, ECpM)
-}
-
-//console.log(ivStaminaFromHP(62))
-//console.log(ivAttackDefenseFromCP(596, 14))
-
-//console.log(findPokemon('bulbasaur'))
-
 // Ok mostly works...
 //console.log(calculate('bulbasaur', 596, 62, 20))
-console.log(calculate('growlithe', 666, 65, 19))
-//console.log(calcIVS('growlithe', 65, 19))
+//console.log(calculate('growlithe', 666, 65, 19))
+console.log(calculate('omastar', 1622, 103, 4000, 25.5))
