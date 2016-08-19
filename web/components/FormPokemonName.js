172103a585b270e070145ff1f0854753a2846c1b
Josh Perez <josh@goatslacker.com>
2016-08-18 19:52:06 -0700

    Let you search with spaces

--- web/components/FormPokemonName.js
+++ web/components/FormPokemonName.js
@@ -4,7 +4,7 @@ const Select = require('react-select')
 const n = require('../utils/n')
 const pokemonActions = require('../actions/pokemonActions')
 
-const options = Pokemon.map(x => ({ value: x.name, label: x.name }))
+const options = Pokemon.map(x => ({ value: x.name.replace(/_/g, ' '), label: x.name }))
 const logName = x => pokemonActions.changedName(x.value)
 
 function FormPokemonName(props) {
