d19ec59f4ac92dc9ff81c0b614871f02faf198b0
Josh Perez <josh@goatslacker.com>
2016-09-20 14:49:00 -0700

    grade overall on a curve

--- web/utils/baseCP.js
+++ web/utils/baseCP.js
@@ -1,5 +1,7 @@
-module.exports = x => (
+const baseCP = x => (
   x.stats.attack *
   Math.pow(x.stats.defense, 0.5) *
   Math.pow(x.stats.stamina, 0.5)
 )
+
+module.exports = baseCP
--- web/utils/ovRating.js
+++ web/utils/ovRating.js
@@ -1,10 +1,19 @@
 const baseCP = require('./baseCP')
+const Pokemon = require('../../json/pokemon')
 
-const percentInRange = (num, min, max) => ((num - min) * 100) / (max - min)
+const poke = Pokemon.reduce((obj, mon) => {
+  const cp = Math.log(baseCP(mon))
+  return {
+    max: Math.max(obj.max, cp),
+    min: Math.min(obj.min, cp),
+  }
+}, {
+  max: -Infinity,
+  min: Infinity,
+})
 
-const MEWTWO_OV = 58771
-const CATERPIE_OV = 4778
+const SCALE = (poke.max - poke.min) / 100
 
-const ovRating = mon => percentInRange(baseCP(mon), CATERPIE_OV, MEWTWO_OV)
+const ovRating = mon => (Math.log(baseCP(mon)) - poke.min) / SCALE
 
 module.exports = ovRating
