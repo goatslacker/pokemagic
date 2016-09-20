6b53d74d96db53015acf205606837a6152c45aea
Josh Perez <josh@goatslacker.com>
2016-09-20 14:10:54 -0700

    Validation

--- web/utils/deux.js
+++ web/utils/deux.js
@@ -1,16 +1,26 @@
 const redux = require('redux')
 
+// TODO put all errors and validation behind a dev flag
+
 const createActionCreators = (arr) => {
   return arr.reduce((obj, type) => {
+    if (typeof type !== 'string') {
+      throw new TypeError('An unknown action type was passed in. Action types must be strings.')
+    }
+
     const name = type.toLowerCase().replace(/_(\w)/g, (a, b) => b.toUpperCase())
 
-    if (obj[name]) throw new ReferenceError(`${name} already exists`)
-    if (obj[type]) throw new ReferenceError(`${type} already exists`)
+    if (obj.creators[name]) throw new ReferenceError(`Action type "${name}" already exists.`)
+    if (obj.types[type]) throw new ReferenceError(`Action type "${type}" already exists.`)
 
-    obj[name] = payload => ({ type, payload })
-    obj[type] = type
+    obj.creators[name] = payload => ({ type, payload })
+    obj.types[type] = type
+
     return obj
-  }, {})
+  }, {
+    creators: {},
+    types: {},
+  })
 }
 
 const mergeState = (initialState, handlers) => {
@@ -27,31 +37,65 @@ const mergeState = (initialState, handlers) => {
   }
 }
 
-const validateActions = (key, actions, handlers) => {
-  const invalid = Object.keys(handlers).filter(x => !actions.hasOwnProperty(x))
-  if (invalid.length) {
-    throw new ReferenceError(`The reducer '${key}' has undefined actions: ${invalid.join(', ')}.`)
-  }
-  return handlers
-}
-
-const createReducer = (key, bundle, actionCreators) => mergeState(
-  bundle.getInitialState(),
-  validateActions(key, actionCreators, bundle.reducers)
+const createReducer = (initialState, reducers) => mergeState(
+  initialState,
+  reducers
 )
 
-const createStore = (reducers, actionCreators) => redux.createStore(
+const createStore = reducers => redux.createStore(
   redux.combineReducers(
     Object.keys(reducers).reduce((o, key) => {
-      o[key] = createReducer(key, reducers[key], actionCreators)
+      const reducer = reducers[key]
+      o[key] = createReducer(reducer.getInitialState(), reducer.reducers)
       return o
     }, {})
   )
 )
 
+const validate = (action, reducers) => {
+  const unhandledActions = Object.keys(reducers).reduce((obj, key) => {
+    const reducer = reducers[key]
+
+    if (typeof reducer.getInitialState !== 'function') {
+      throw new TypeError(`The reducer ${key} does not have getInitialState defined or it is not a function.`)
+    }
+    if (typeof reducer.reducers !== 'object') {
+      throw new TypeError(`The reducer ${key} does not have reducers defined or it is not an Object.`)
+    }
+
+    const undefinedActions = Object.keys(reducer.reducers).filter((x) => {
+      if (action.types.hasOwnProperty(x)) {
+        delete obj[x]
+        return false
+      }
+      return true
+    })
+    if (undefinedActions.length) {
+      throw new ReferenceError(`The reducer '${key}' has undefined actions: ${undefinedActions.join(', ')}.`)
+    }
+    return obj
+  }, Object.assign({}, action.types))
+
+  const unhandledActionsKeys = Object.keys(unhandledActions)
+
+  if (unhandledActionsKeys.length) {
+    throw new ReferenceError(`The following actions are defined but not used: ${unhandledActionsKeys.join(', ')}.`)
+  }
+}
+
 module.exports = (actionTypes, reducers) => {
-  const actionCreators = createActionCreators(actionTypes)
-  const store = createStore(reducers, actionCreators)
-  const dispatch = redux.bindActionCreators(actionCreators, store.dispatch)
-  return { actionCreators, dispatch, redux, store }
+  if (!reducers) throw new TypeError('"reducers" must be an Object.')
+  if (!Array.isArray(actionTypes)) throw new TypeError('"actionTypes" must be an Array.')
+  const action = createActionCreators(actionTypes)
+
+  validate(action, reducers)
+
+  const store = createStore(reducers)
+  const dispatch = redux.bindActionCreators(action.creators, store.dispatch)
+  return {
+    actionCreators: action.creators,
+    dispatch,
+    redux,
+    store,
+  }
 }
