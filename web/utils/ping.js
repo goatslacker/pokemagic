399ee54aefd69fb42e8612145751be335306639e
Josh Perez <josh@goatslacker.com>
2016-10-06 19:38:57 -0700

    rm consolelog

--- web/utils/ping.js
+++ web/utils/ping.js
@@ -2,8 +2,6 @@ function ping(check, f, time) {
   if (time > 5000) return
 
   setTimeout(() => {
-    console.log('@CHECKING', check())
-
     if (typeof check() !== 'undefined') f()
     else ping(check, f, (time || 10) * 1.5)
   }, time || 10)
