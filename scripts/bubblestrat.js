147e52478826a1287fb10a22f5c0efe772e1ea22
Josh Perez <josh@goatslacker.com>
2016-12-29 11:31:37 -0800

    bubblestrating

--- scripts/bubblestrat.js
+++ scripts/bubblestrat.js
@@ -10,11 +10,10 @@ function getDmgVs(obj) {
   const moves = obj.moves
   const player = obj.player
   const opponent = obj.opponent
-  const pokemonLevel = obj.pokemonLevel || 25
 
-  // We determine DPS combo vs a level 25 pokemon because that's what gyms on average will have.
-  // There's no hard data for this claim btw, it's completely made up.
-  const opponentLevel = obj.opponentLevel || 25
+  // TODO make these dynamic
+  const pokemonLevel = 1.5
+  const opponentLevel = 1.5
 
   const AtkECpM = LevelToCPM[pokemonLevel]
   const DefECpM = LevelToCPM[opponentLevel]
@@ -26,107 +25,122 @@ function getDmgVs(obj) {
     const fxMul = getTypeEffectiveness(opponent, move)
 
     return {
+      poke: player,
       name: move.Name,
       duration: move.DurationMs,
-      dmg: (0.5 * atk * AtkECpM / (def * DefECpM) * power * stab * fxMul) + 1,
+      dmg: Math.floor((0.5 * atk * AtkECpM / (def * DefECpM) * power * stab * fxMul) + 1),
     }
   })
-}
-
-function cpFor(x) {
-  return cpTools.getCP(x.pokemon, {
-    atk: x.IndAtk,
-    def: x.IndDef,
-    sta: x.IndSta,
-  }, LevelToCPM[x.level])
 }
 
 function hpFor(x) {
-  return hpTools.getHP(x.pokemon, x.IndSta, LevelToCPM[x.level])
-}
-
-// TODO not bubblestrat but we can perhaps figure out a ranking for training up
-// a gym while using the least possible potions or with minimal amount of dodging
-// call it the bubblestrat lite
-function vs(a, b) {
-  const atk = a.pokemon.stats.attack + a.IndAtk
-  const def = b.pokemon.stats.defense + b.IndDef
-
-  const oppHp = hpFor(b)
-
-  return b.pokemon.moves1.map(move => {
-    return {
-      gym: b.pokemon.name,
-      vs: move.Name,
-      moves: getDmgVs({
-        atk,
-        def,
-        player: a.pokemon,
-        opponent: b.pokemon,
-        pokemonLevel: a.level,
-        opponentLevel: b.level,
-        moves: a.pokemon.moves1,
-      })
-      // how many hits can you land before the opponent gets theirs off
-      .map(x => {
-        const howManyHits = Math.floor(move.DurationMs / x.duration)
-        return {
-          trainer: a.pokemon.name,
-          name: x.name,
-          dmg: x.dmg * howManyHits,
-        }
-      })
-      // Can we destroy the opponent before they land a hit...
-      .filter(x => x.dmg > oppHp)
-    }
-  })
-  // filter out unviable moves
-  .filter(x => x.moves.length > 0)
+  return hpTools.getHP(x.pokemon, x.IndSta, LevelToCPM[x.level]) * 2
 }
 
 function createTrainer(pokemon) {
   return {
     pokemon,
-    level: 1,
-    IndAtk: 10,
-    IndDef: 10,
-    IndSta: 10,
+    level: 1.5,
+    IndAtk: 7,
+    IndDef: 7,
+    IndSta: 7,
   }
 }
 
+// TODO kill this function
+// we need variable levels
 function createGym(pokemon) {
   return {
     pokemon,
-    // XXX also do different levels...
     level: 1.5,
-    IndAtk: 10,
-    IndDef: 10,
-    IndSta: 10,
+    IndAtk: 7,
+    IndDef: 7,
+    IndSta: 7,
   }
 }
 
 function vFor(name) {
   const b = createGym(Pokemon.filter(x => x.name === name.toUpperCase())[0])
-  return Pokemon.map(trainer => {
-    const a = createTrainer(trainer)
-    return vs(a, b)
-  }).filter(x => x.length > 0)
+  const def = b.pokemon.stats.defense + b.IndDef
+  const oppHp = hpFor(b)
+  const oppCp = cpTools.getCP(b.pokemon, {
+    atk: 7,
+    def: 7,
+    sta: 7,
+  }, LevelToCPM[1.5])
+
+  return b.pokemon.moves1.map(move => {
+    return {
+      bubbler: b.pokemon.name,
+      move: move.Name,
+      cp: oppCp,
+      hp: oppHp,
+
+      attackers: Pokemon
+//      .filter(x => x.name === 'DIGLETT')
+      .reduce((arr, trainer) => {
+        const a = createTrainer(trainer)
+        const atk = a.pokemon.stats.attack + a.IndAtk
+
+        return arr.concat(getDmgVs({
+          atk,
+          def,
+          player: a.pokemon,
+          opponent: b.pokemon,
+          moves: a.pokemon.moves1,
+        }))
+      }, [])
+      // calculate total damage output for entire fight
+      .map(x => {
+        const howManyHits = Math.floor((move.DurationMs + 1000) / x.duration)
+        return {
+          name: x.poke.name,
+          move: x.name,
+          dmg: x.dmg * howManyHits,
+          cp: cpTools.getCP(x.poke, {
+            atk: 7,
+            def: 7,
+            sta: 7,
+          }, LevelToCPM[1.5])
+        }
+      })
+      // how many hits can you land before the opponent gets theirs off
+      // Can we destroy the opponent before they land a hit...
+      .filter(x => x.dmg >= oppHp)
+      // calculate the prestige gain
+      .map(x => ({
+        n: x.name,
+        m: x.move,
+        cp: x.cp,
+        prestige: oppCp > x.cp
+          ? Math.min(1000, Math.floor(500 * (oppCp / x.cp)))
+          : Math.min(100, Math.floor(310 * (oppCp / x.cp)) - 55),
+      }))
+      .filter(x => x.prestige >= 500)
+      // sort by most prestige
+      .sort((a, b) => a.prestige > b.prestige ? -1 : 1)
+    }
+  })
+  .filter(x => x.attackers.length > 0)
 }
 
-// Specific to bubblestrat only.
-const TRAINER = createTrainer(Pokemon.filter(x => x.name === 'DIGLETT')[0])
-const GYM = createGym(Pokemon.filter(x => x.name === 'HORSEA')[0])
+//const DIGLETT = Pokemon.filter(x => x.name === 'DIGLETT')[0]
+//const POLIWAG = Pokemon.filter(x => x.name === 'POLIWAG')[0]
 
-// All possible Pokemon
-const all = Pokemon.map(trainer => {
-  const a = createTrainer(trainer)
-  return Pokemon.map(gym => {
-    const b = createGym(gym)
-    return vs(a, b)
-  }).filter(x => x.length > 0)
-}).filter(x => x.length > 0)
+//console.log('?',
+//  getDmgVs({
+//    atk: DIGLETT.stats.attack,
+//    def: POLIWAG.stats.defense,
+//    moves: [DIGLETT.moves1[2]],
+//    player: DIGLETT,
+//    opponent: POLIWAG,
+//  }),
+//  'hits', Math.floor((2300 + 1000) / 500),
+//  6 * 4,
+//  hpFor({ pokemon: POLIWAG, IndSta: 1, level: 1.5 })
+//)
 
-//console.log(JSON.stringify(vs(TRAINER, GYM)))
-//console.log(JSON.stringify(all))
-
-console.log(JSON.stringify(vFor('krabby')))
+console.log(
+//  vFor('poliwag')[0]
+  vFor('horsea')[0]
+)
