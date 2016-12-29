23ef42ee5994a5130a847f5a101412f7e62a3353
Josh Perez <josh@goatslacker.com>
2016-12-29 11:55:34 -0800

    bubblestrating

--- scripts/bubblestrat.js
+++ scripts/bubblestrat.js
@@ -1,8 +1,31 @@
 const Pokemon = require('../json/pokemon')
 const getTypeEffectiveness = require('../src/getTypeEffectiveness').getTypeEffectiveness
-const cpTools = require('../src/cp')
-const hpTools = require('../src/hp')
-const LevelToCPM = require('../json/level-to-cpm')
+
+const LevelToCPM = {
+  "1": 0.094, "1.5": 0.135137432, "2": 0.16639787, "2.5": 0.192650919, "3": 0.21573247, "3.5": 0.236572661, "4": 0.25572005, "4.5": 0.273530381, "5": 0.29024988, "5.5": 0.306057377, "6": 0.3210876, "6.5": 0.335445036, "7": 0.34921268, "7.5": 0.362457751, "8": 0.37523559, "8.5": 0.387592406, "9": 0.39956728, "9.5": 0.411193551, "10": 0.42250001, "10.5": 0.432926419, "11": 0.44310755, "11.5": 0.4530599578, "12": 0.46279839, "12.5": 0.472336083, "13": 0.48168495, "13.5": 0.4908558, "14": 0.49985844, "14.5": 0.508701765, "15": 0.51739395, "15.5": 0.525942511, "16": 0.53435433, "16.5": 0.542635767, "17": 0.55079269, "17.5": 0.558830576, "18": 0.56675452, "18.5": 0.574569153, "19": 0.58227891, "19.5": 0.589887917, "20": 0.59740001, "20.5": 0.604818814, "21": 0.61215729, "21.5": 0.619399365, "22": 0.62656713, "22.5": 0.633644533, "23": 0.64065295, "23.5": 0.647576426, "24": 0.65443563, "24.5": 0.661214806, "25": 0.667934, "25.5": 0.674577537, "26": 0.68116492, "26.5": 0.687680648, "27": 0.69414365, "27.5": 0.700538673, "28": 0.70688421, "28.5": 0.713164996, "29": 0.71939909, "29.5": 0.725571552, "30": 0.7317, "30.5": 0.734741009, "31": 0.73776948, "31.5": 0.740785574, "32": 0.74378943, "32.5": 0.746781211, "33": 0.74976104, "33.5": 0.752729087, "34": 0.75568551, "34.5": 0.758630378, "35": 0.76156384, "35.5": 0.764486065, "36": 0.76739717, "36.5": 0.770297266, "37": 0.7731865, "37.5": 0.776064962, "38": 0.77893275, "38.5": 0.781790055, "39": 0.78463697, "39.5": 0.787473578, "40": 0.79030001
+}
+
+function getHP(mon, IndSta, ECpM) {
+  const BaseSta = mon.stats.stamina
+  return Math.max(10, Math.floor(ECpM * (BaseSta + IndSta)))
+}
+
+function getCP(mon, ivs, ECpM) {
+  const BaseAtk = mon.stats.attack
+  const BaseDef = mon.stats.defense
+  const BaseSta = mon.stats.stamina
+  const IndAtk = ivs.atk
+  const IndDef = ivs.def
+  const IndSta = ivs.sta
+
+  return Math.max(10, Math.floor(
+    (BaseAtk + IndAtk) *
+    Math.pow(BaseDef + IndDef, 0.5) *
+    Math.pow(BaseSta + IndSta, 0.5) *
+    Math.pow(ECpM, 2) /
+    10
+  ))
+}
 
 function getDmgVs(obj) {
   const atk = obj.atk
@@ -10,10 +33,8 @@ function getDmgVs(obj) {
   const moves = obj.moves
   const player = obj.player
   const opponent = obj.opponent
-
-  // TODO make these dynamic
-  const pokemonLevel = 1.5
-  const opponentLevel = 1.5
+  const pokemonLevel = obj.pokemonLevel || 1.5
+  const opponentLevel = obj.opponentLevel || 1.5
 
   const AtkECpM = LevelToCPM[pokemonLevel]
   const DefECpM = LevelToCPM[opponentLevel]
@@ -33,61 +54,38 @@ function getDmgVs(obj) {
   })
 }
 
-function hpFor(x) {
-  return hpTools.getHP(x.pokemon, x.IndSta, LevelToCPM[x.level]) * 2
-}
+const getBubblerHP = (poke, IndSta, level) => (
+  getHP(poke, IndSta, LevelToCPM[level]) * 2
+)
 
-function createTrainer(pokemon) {
-  return {
-    pokemon,
-    level: 1.5,
-    IndAtk: 7,
-    IndDef: 7,
-    IndSta: 7,
-  }
-}
+function bubble(opponent, pokemon) {
+  const name = opponent.name
+  const ivs = opponent.ivs
+  const opponentLevel = opponent.level
 
-// TODO kill this function
-// we need variable levels
-function createGym(pokemon) {
-  return {
-    pokemon,
-    level: 1.5,
-    IndAtk: 7,
-    IndDef: 7,
-    IndSta: 7,
-  }
-}
+  const pokemonLevel = pokemon.level
+  const pokeIvs = pokemon.ivs
 
-function vFor(name) {
-  const b = createGym(Pokemon.filter(x => x.name === name.toUpperCase())[0])
-  const def = b.pokemon.stats.defense + b.IndDef
-  const oppHp = hpFor(b)
-  const oppCp = cpTools.getCP(b.pokemon, {
-    atk: 7,
-    def: 7,
-    sta: 7,
-  }, LevelToCPM[1.5])
+  const bubbler = Pokemon.filter(x => x.name === name.toUpperCase())[0]
+  const oppHP = getBubblerHP(bubbler, ivs.sta, opponentLevel)
+  const oppCP = getCP(bubbler, ivs, LevelToCPM[opponentLevel])
 
-  return b.pokemon.moves1.map(move => {
+  return bubbler.moves1.map(move => {
     return {
-      bubbler: b.pokemon.name,
+      bubbler: bubbler.name,
       move: move.Name,
-      cp: oppCp,
-      hp: oppHp,
+      cp: oppCP,
+      hp: oppHP,
 
-      attackers: Pokemon
-//      .filter(x => x.name === 'DIGLETT')
-      .reduce((arr, trainer) => {
-        const a = createTrainer(trainer)
-        const atk = a.pokemon.stats.attack + a.IndAtk
-
+      attackers: Pokemon.reduce((arr, trainer) => {
         return arr.concat(getDmgVs({
-          atk,
-          def,
-          player: a.pokemon,
-          opponent: b.pokemon,
-          moves: a.pokemon.moves1,
+          atk: trainer.stats.attack + pokeIvs.atk,
+          def: bubbler.stats.defense + ivs.def,
+          player: trainer,
+          opponent: bubbler,
+          pokemonLevel,
+          opponentLevel,
+          moves: trainer.moves1,
         }))
       }, [])
       // calculate total damage output for entire fight
@@ -97,50 +95,38 @@ function vFor(name) {
           name: x.poke.name,
           move: x.name,
           dmg: x.dmg * howManyHits,
-          cp: cpTools.getCP(x.poke, {
-            atk: 7,
-            def: 7,
-            sta: 7,
-          }, LevelToCPM[1.5])
+          cp: getCP(x.poke, pokeIvs, LevelToCPM[pokemonLevel])
         }
       })
       // how many hits can you land before the opponent gets theirs off
       // Can we destroy the opponent before they land a hit...
-      .filter(x => x.dmg >= oppHp)
+      .filter(x => x.dmg >= oppHP)
       // calculate the prestige gain
       .map(x => ({
         n: x.name,
         m: x.move,
         cp: x.cp,
-        prestige: oppCp > x.cp
-          ? Math.min(1000, Math.floor(500 * (oppCp / x.cp)))
-          : Math.min(100, Math.floor(310 * (oppCp / x.cp)) - 55),
+        p: oppCP > x.cp
+          ? Math.min(1000, Math.floor(500 * (oppCP / x.cp)))
+          : Math.min(100, Math.floor(310 * (oppCP / x.cp)) - 55),
       }))
-      .filter(x => x.prestige >= 500)
+      .filter(x => x.p >= 500)
       // sort by most prestige
-      .sort((a, b) => a.prestige > b.prestige ? -1 : 1)
+      .sort((a, b) => a.p > b.p ? -1 : 1)
     }
   })
   .filter(x => x.attackers.length > 0)
 }
 
-//const DIGLETT = Pokemon.filter(x => x.name === 'DIGLETT')[0]
-//const POLIWAG = Pokemon.filter(x => x.name === 'POLIWAG')[0]
-
-//console.log('?',
-//  getDmgVs({
-//    atk: DIGLETT.stats.attack,
-//    def: POLIWAG.stats.defense,
-//    moves: [DIGLETT.moves1[2]],
-//    player: DIGLETT,
-//    opponent: POLIWAG,
-//  }),
-//  'hits', Math.floor((2300 + 1000) / 500),
-//  6 * 4,
-//  hpFor({ pokemon: POLIWAG, IndSta: 1, level: 1.5 })
-//)
-
 console.log(
-//  vFor('poliwag')[0]
-  vFor('horsea')[0]
+  bubble({
+    // bubbler
+    name: 'horsea',
+    ivs: { atk: 7, def: 3, sta: 3 },
+    level: 1.5,
+  }, {
+    // attacker's stats
+    ivs: { atk: 7, def: 3, sta: 3 },
+    level: 1.5,
+  })[0]
 )
