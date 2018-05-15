6d751df50a05cfec26a6660ce3f86f050c0b4a7e
Josh Perez <josh@goatslacker.com>
2018-05-14 19:15:52 -0700

    Update URL for game master source

--- scripts/GameMaster.js
+++ scripts/GameMaster.js
@@ -2,6 +2,6 @@
 const axios = require('axios');
 
 const URL =
-  'https://raw.githubusercontent.com/BrunnerLivio/pokemongo-game-master/master/versions/latest/GAME_MASTER.json';
+  'https://raw.githubusercontent.com/pokemongo-dev-contrib/pokemongo-game-master/master/versions/latest/GAME_MASTER.json';
 
 module.exports = axios.get(URL).then(res => res.data);
